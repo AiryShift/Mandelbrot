5e11211fbf0bbe96830343389f7018d2258a9dae
AiryShift <religionofaperturelabs@gmail.com>
2015-04-10 00:04:12 +1000

    Write functions escapeSteps etc. to working status
    escapeSteps calulates the number of iterations before a term escapes the set
    Escaping is given thatthe distance from P(x, y) and the origin is >= 2
    It is given that all terms start at 1 iteration

--- mandelbrot.c
+++ mandelbrot.c
@@ -4,33 +4,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "mandelbrot.h"
 
-#define MAX_ITERATIONS 9001 // Placeholder
-#define ORIGINAL_VALUE_COUNT 2
+#define MAX_ITERATIONS 256
 
+/*
 int main(int argc, char *argv[]) {
-    /* code */
     return EXIT_SUCCESS;
 }
+*/
 
 int escapeSteps(double x, double y) {
-    double originalX = x;
-    double originalY = y;
-    int iterations = 0;
+    double firstX = x;
+    double firstY = y;
+    int iterations = 1; // Mandelbrot assumes minimum 1 iteration
 
-    while (/* Not infinity */ || iterations < MAX_ITERATIONS) {
-        nextTerm(&x, &y, originalX, originalY);
+    while (isBounded(x, y) && iterations < MAX_ITERATIONS) {
+        nextTerm(&x, &y, firstX, firstY);
         iterations++;
     }
+    return iterations;
 }
 
-void nextTerm(double *x, double *y, double originalX, double originalY) {
-    square(x, y);
-    *x += originalX;
-    *y += originalY;
+int isBounded(double x, double y) {
+    int bounded = 1;
+    if (root(square(x) + square(y)) >= 2) { // Distance formula
+        bounded = 0;
+    }
+    return bounded;
 }
 
-void square(double *x, double *y) {
-    // (x + y)**2 = x**2 + 2xy + y**2
+void nextTerm(double *x, double *y, double firstX, double firstY) {
+    complexSquare(x, y);
+    *x += firstX;
+    *y += firstY;
+}
+
+void complexSquare(double *x, double *y) {
+    // (x + yi)**2 = (x**2 - y**2) + 2xyi
+    double tempX = *x;
+    double tempY = *y;
+    *x = square(tempX) - square(tempY);
+    *y = 2 * tempX * tempY;
+}
+
+double square(double x) {
+    return pow(x, 2);
+}
+
+double root(double x) {
+    return pow(x, 0.5);
 }
