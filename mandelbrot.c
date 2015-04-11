22b5060c23298787b16a640ac697be90f4cc939b
AiryShift <religionofaperturelabs@gmail.com>
2015-04-11 22:12:28 +1000

    Add BMP header function to mandelbrot.c
    Comment out all output as it needs to be redirected to the function
    that writes to the server

--- mandelbrot.c
+++ mandelbrot.c
@@ -28,14 +28,30 @@ void serveBMP(int socket);
 int makeServerSocket(int portno);
 int waitForConnection(int serverSocket);
 
-int main(int argc, char *argv[]) {
-    printf ("************************************\n");
-    printf ("Starting simple server %f\n", SIMPLE_SERVER_VERSION);
-    printf ("Serving bmps since 2012\n");
+// BMP
+#define BYTES_PER_PIXEL 3
+#define BITS_PER_PIXEL (BYTES_PER_PIXEL*8)
+#define NUMBER_PLANES 1
+#define PIX_PER_METRE 2835
+#define MAGIC_NUMBER 0x4d42
+#define NO_COMPRESSION 0
+#define OFFSET 54
+#define DIB_HEADER_SIZE 40
+#define NUM_COLORS 0
 
-    int serverSocket = makeServerSocket (DEFAULT_PORT);
-    printf ("Access this server at http://localhost:%d/\n", DEFAULT_PORT);
-    printf ("************************************\n");
+#define SIZE 512
+#define TOTAL_NUM_BYTES (SIZE*SIZE*BYTES_PER_PIXEL)
+
+void writeHeader(void);
+
+int main(int argc, char *argv[]) {
+    printf("************************************\n");
+    printf("Starting simple server %f\n", SIMPLE_SERVER_VERSION);
+    printf("Serving bmps since 2012\n");
+
+    int serverSocket = makeServerSocket(DEFAULT_PORT);
+    printf("Access this server at http://localhost:%d/\n", DEFAULT_PORT);
+    printf("************************************\n");
 
     char request[REQUEST_BUFFER_SIZE];
 
@@ -43,21 +59,21 @@ int main(int argc, char *argv[]) {
     while (numberServed < NUMBER_OF_PAGES_TO_SERVE) {
         printf ("*** So far served %d pages ***\n", numberServed);
 
-        int connectionSocket = waitForConnection (serverSocket);
+        int connectionSocket = waitForConnection(serverSocket);
         // wait for a request to be sent from a web browser, open a new
         // connection for this conversation
 
         // read the first line of the request sent by the browser
         int bytesRead;
-        bytesRead = read (connectionSocket, request, (sizeof request)-1);
-        assert (bytesRead >= 0);
+        bytesRead = read(connectionSocket, request, (sizeof request) - 1);
+        assert(bytesRead >= 0);
         // were we able to read any data from the connection?
 
         // print entire request to the console
-        printf (" *** Received http request ***\n %s\n", request);
+        printf(" *** Received http request ***\n %s\n", request);
 
         //send the browser a simple html page using http
-        printf (" *** Sending http response ***\n");
+        printf(" *** Sending http response ***\n");
         serveBMP(connectionSocket);
 
         // close the connection after sending the page- keep aust beautiful
@@ -113,15 +129,15 @@ static double square(double x) {
 }
 
 
-void serveBMP (int socket) {
+void serveBMP(int socket) {
     char* message;
 
     // first send the http response header
     message = "HTTP/1.0 200 OK\r\n"
               "Content-Type: image/bmp\r\n"
               "\r\n";
-    printf ("about to send=> %s\n", message);
-    write (socket, message, strlen (message));
+    printf("about to send=> %s\n", message);
+    write(socket, message, strlen(message));
 
     // now send the BMP
     unsigned char bmp[] = {
@@ -138,27 +154,27 @@ void serveBMP (int socket) {
         0x07,0x07,0x07,0x07,0xff,0xff,0xff,0x00,
         0x00,0x0d};
 
-    write (socket, bmp, sizeof(bmp));
+    write(socket, bmp, sizeof(bmp));
 }
 
 // start the server listening on the specified port number
-int makeServerSocket (int portNumber) {
+int makeServerSocket(int portNumber) {
     // create socket
-    int serverSocket = socket (AF_INET, SOCK_STREAM, 0);
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    assert (serverSocket >= 0); // error opening socket
+    assert(serverSocket >= 0); // error opening socket
 
     // bind socket to listening port
     struct sockaddr_in serverAddress;
-    bzero ((char *) &serverAddress, sizeof (serverAddress));
+    bzero((char *) &serverAddress, sizeof(serverAddress));
 
     serverAddress.sin_family      = AF_INET;
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port        = htons (portNumber);
+    serverAddress.sin_port        = htons(portNumber);
 
     // let the server start immediately after a previous shutdown
     int optionValue = 1;
-    setsockopt (
+    setsockopt(
         serverSocket,
         SOL_SOCKET,
         SO_REUSEADDR,
@@ -167,13 +183,13 @@ int makeServerSocket (int portNumber) {
     );
 
     int bindSuccess =
-        bind (
+        bind(
             serverSocket,
             (struct sockaddr *) &serverAddress,
             sizeof (serverAddress)
         );
 
-    assert (bindSuccess >= 0);
+    assert(bindSuccess >= 0);
     // if this assert fails wait a short while to let the operating
     // system clear the port before trying again
 
@@ -182,22 +198,72 @@ int makeServerSocket (int portNumber) {
 
 // wait for a browser to request a connection,
 // returns the socket on which the conversation will take place
-int waitForConnection (int serverSocket) {
+int waitForConnection(int serverSocket) {
     // listen for a connection
     const int serverMaxBacklog = 10;
-    listen (serverSocket, serverMaxBacklog);
+    listen(serverSocket, serverMaxBacklog);
 
     // accept the connection
     struct sockaddr_in clientAddress;
-    socklen_t clientLen = sizeof (clientAddress);
+    socklen_t clientLen = sizeof(clientAddress);
     int connectionSocket =
-        accept (
+        accept(
             serverSocket,
-            (struct sockaddr *) &clientAddress,
+           (struct sockaddr *) &clientAddress,
             &clientLen
         );
 
-    assert (connectionSocket >= 0); // error on accept
+    assert(connectionSocket >= 0); // error on accept
 
-    return (connectionSocket);
+    return(connectionSocket);
+}
+
+void writeHeader(void) {
+    // From chessboard activity
+    /*
+    unsigned short magicNumber = MAGIC_NUMBER;
+    printf("% \n", magicNumber);
+
+    unsigned int fileSize = OFFSET + (SIZE * SIZE * BYTES_PER_PIXEL);
+    printf("% \n", fileSize);
+
+    unsigned int reserved = 0;
+    printf("% \n", reserved);
+
+    unsigned int offset = OFFSET;
+    printf("% \n", offset);
+
+    unsigned int dibHeaderSize = DIB_HEADER_SIZE;
+    printf("% \n", dibHeaderSize);
+
+    unsigned int width = SIZE;
+    printf("% \n", width);
+
+    unsigned int height = SIZE;
+    printf("% \n", height);
+
+    unsigned short planes = NUMBER_PLANES;
+    printf("% \n", planes);
+
+    unsigned short bitsPerPixel = BITS_PER_PIXEL;
+    printf("% \n", bitsPerPixel);
+
+    unsigned int compression = NO_COMPRESSION;
+    printf("% \n", compression);
+
+    unsigned int imageSize = (SIZE * SIZE * BYTES_PER_PIXEL);
+    printf("% \n", imageSize);
+
+    unsigned int hResolution = PIX_PER_METRE;
+    printf("% \n", hResolution);
+
+    unsigned int vResolution = PIX_PER_METRE;
+    printf("% \n", vResolution);
+
+    unsigned int numColors = NUM_COLORS;
+    printf("% \n", numColors);
+
+    unsigned int importantColors = NUM_COLORS;
+    printf("% \n", importantColors);
+    */
 }
