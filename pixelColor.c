53e645c890095d567dc5f0d9ed4b15def49e28e5
AiryShift <religionofaperturelabs@gmail.com>
2015-04-11 16:42:31 +1000

    Create pixelColor.c
    Contains 3 definitions for the 3 declarations in pixelColor.h

---
+++ pixelColor.c
@@ -0,0 +1,16 @@
+/*
+    Calculates the RGB values of a pixel given steps taken to escape
+ */
+
+unsigned char stepsToRed(int steps) {
+    // Some algorithm
+    return 0;
+}
+unsigned char stepsToGreen(int steps) {
+    // Some algorithm
+    return 0;
+}
+unsigned char stepsToBlue(int steps) {
+    // Some algorithm
+    return 0;
+}
