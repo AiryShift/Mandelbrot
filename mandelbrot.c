5439648be28b3357cff992768ffe55437c70f993
AiryShift <religionofaperturelabs@gmail.com>
2015-04-14 23:06:09 +1000

    Add complex number struct

--- mandelbrot.c
+++ mandelbrot.c
@@ -7,44 +7,56 @@
 #define MAX_ITERATIONS 256
 #define ESCAPE_DISTANCE 2
 
-static int isBounded(double x, double y);
-static void nextTerm(double *x, double *y, double initX, double initY);
-static void complexSquare(double *x, double *y);
+typedef struct _complex {
+    double real;
+    double imaginary;
+} complex;
+
+static int isBounded(complex num);
+static double distanceFromOrigin(complex num);
+static complex nextTerm(complex num, complex initial);
+static complex complexSquare(complex num);
 static double square(double x);
 
 int escapeSteps(double x, double y) {
-    double initX = x;
-    double initY = y;
+    complex initialValue = {x, y};
+    complex workingCopy = initialValue;
     int iterations = MIN_ITERATIONS; // Mandelbrot assumes this
 
-    while (isBounded(x, y) && iterations < MAX_ITERATIONS) {
-        nextTerm(&x, &y, initX, initY);
+    while (isBounded(workingCopy) && iterations < MAX_ITERATIONS) {
+        workingCopy = nextTerm(workingCopy, initialValue);
         iterations++;
     }
     return iterations;
 }
 
-static int isBounded(double x, double y) {
+static int isBounded(complex num) {
     int bounded = 1;
-    if (sqrt(square(x) + square(y)) >= ESCAPE_DISTANCE) { // Distance f
+    if (distanceFromOrigin(num) >= ESCAPE_DISTANCE) {
         bounded = 0;
     }
     return bounded;
 }
 
-static void nextTerm(double *x, double *y, double initX, double initY) {
-    complexSquare(x, y);
-    *x += initX;
-    *y += initY;
+static double distanceFromOrigin(complex num) {
+    // Distance formula
+    return sqrt(square(num.real) + square(num.imaginary));
 }
 
-static void complexSquare(double *x, double *y) {
+static complex nextTerm(complex num, complex initial) {
+    num = complexSquare(num);
+    num.real += initial.real;
+    num.imaginary += initial.imaginary;
+    return num;
+}
+
+static complex complexSquare(complex num) {
     // The square of a complex number resolves to:
     // (x + yi)**2 = (x**2 - y**2) + (2xy)i
-    double tempX = *x;
-    double tempY = *y;
-    *x = square(tempX) - square(tempY);
-    *y = 2 * tempX * tempY;
+    complex temp = num;
+    num.real = square(temp.real) - square(temp.imaginary);
+    num.imaginary = 2 * temp.real * temp.imaginary;
+    return num;
 }
 
 static double square(double x) {
