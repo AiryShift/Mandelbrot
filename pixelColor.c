78f5c50aa62a5e26f4f5e4ea9d2cf0e303dad3b3
AiryShift <religionofaperturelabs@gmail.com>
2015-04-25 13:41:47 +1000

    Add -1 to all colors because BMP maxval is 255

--- pixelColor.c
+++ pixelColor.c
@@ -1,13 +1,11 @@
-/*
-    Calculates the RGB values of a pixel given steps taken to escape
- */
-
 unsigned char stepsToRed(int steps) {
-    return steps;
+    return steps - 1;
 }
+
 unsigned char stepsToGreen(int steps) {
-    return steps;
+    return steps - 1;
 }
+
 unsigned char stepsToBlue(int steps) {
-    return steps;
+    return steps - 1;
 }
